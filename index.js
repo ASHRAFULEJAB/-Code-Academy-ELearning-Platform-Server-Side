7342d86c6185cb11eff19436c6e3124454e3f08c
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-10-25 03:29:07 +0600

    course-set-done

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
-
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('New course are coming soon')
+const courses = require('./Data/CourseList.json')
+const courseListItem = require('./Data/CourseDetails.json')
+
+app.get('/courses', (req, res) => {
+  res.send(courses)
 })
-app.listen(port,()=> {
-    console.log(`Code academy is running....${port}`)
+app.get('/courseslist/:id', (req, res) => {
+  const id = req.params.id
+
+  const selectedCourseList = courseListItem.filter((cl) => cl.category_id == id)
+
+  res.send(selectedCourseList)
+})
+
+app.get('/', (req, res) => {
+  res.send('New course are coming soon')
+})
+app.listen(port, () => {
+  console.log(`Code academy is running....${port}`)
 })
